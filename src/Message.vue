7d516940049b5fd399b6064548e63f3a038b32c2
kenpeter <figo2476@hotmail.com>
2017-01-03 23:10:10 +1100

    add Message.vue and MyInput.vue

--- src/Message.vue
+++ src/Message.vue
@@ -1,8 +1,9 @@
 <template>
   <!-- one root element -->
   <div>
-    <h1>This is msg</h1>
-    <my-input></my-input>
+    <h1>h1: {{ message }}</h1>
+    <!-- :msg is props -->
+    <my-input :msg="message" @msgChanged="message = $event"></my-input>
   </div>
 </template>
 
@@ -12,6 +13,13 @@
 
   // because it may be used in other place
   export default {
+    // data: func() {}
+    data() {
+      return {
+        message: "pass message into my-input"
+      }
+    },
+  
     // component key word
     components: {
       "my-input": MyInput
--- src/MyInput.vue
+++ src/MyInput.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <!-- 1st way, model changes, then view change -->
-    <input type="text" v-model="message">
+    <!-- v-on:bind == :value -->
+    <!-- @input listening changes -->
+    <input type="text" :value="msg" @input="changeMessage">
     
     <!-- 2nd way, view changed by someone else, the model is changed -->
     <p>{{ message }}</p>
@@ -12,15 +14,27 @@
   // export
   // default, why export default, like export default
   export default {
+    props: ['msg'],
+  
     // es6, es5 is data: function()
     data() {
       // always return
       // this time return json obj.
       // now func becomes props
       return {
-        message: "default msg"
+        message: "default text for input box"
+      }
+    },
+    
+    methods: {
+      changeMessage(event) {
+        this.message = event.target.value;    
+        
+        // This will fire to whoever listen, so can be parent
+        // $ build-in
+        this.$emit("msgChanged", this.message);
       }
     }
-  
+    
   }
 </script>
